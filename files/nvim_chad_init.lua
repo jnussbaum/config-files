c6b0adda3d66f7b6f7b746a799883b6d303ebd84
Johannes Nussbaum <johannes.nussbaum@dasch.swiss>
2025-01-31 16:28:29 +0100

    edit

--- files/nvim_chad_init.lua
+++ files/nvim_chad_init.lua
@@ -3,6 +3,7 @@
 vim.g.base46_cache = vim.fn.stdpath "data" .. "/base46/"
 vim.g.mapleader = " "
 
+
 -- bootstrap lazy and all plugins
 local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
 
@@ -14,6 +15,7 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 local lazy_config = require "configs.lazy"
+
 
 -- load plugins
 require("lazy").setup({
@@ -27,6 +29,7 @@ require("lazy").setup({
   { import = "plugins" },
 }, lazy_config)
 
+
 -- load theme
 dofile(vim.g.base46_cache .. "defaults")
 dofile(vim.g.base46_cache .. "statusline")
@@ -38,5 +41,17 @@ vim.schedule(function()
   require "mappings"
 end)
 
+
 -- change keymap to exit Terminal
 vim.api.nvim_set_keymap('t', '<Esc>', [[<C-\><C-n>]], { noremap = true, silent = true })
+
+
+-- make line numbers relative to current line (current line has absolute number)
+vim.opt.relativenumber = true
+
+
+-- set indentation level to 4 spaces
+vim.opt.expandtab = true      -- Use spaces instead of tabs
+vim.opt.tabstop = 4           -- Number of spaces for a tab
+vim.opt.shiftwidth = 4        -- Number of spaces for indentation
+vim.opt.smartindent = true    -- Enable smart indentation
